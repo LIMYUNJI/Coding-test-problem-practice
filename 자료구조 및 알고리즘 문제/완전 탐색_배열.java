209680cae44bccf6736f2968f8da3b607c9c7ac6
happy <45088924+LIMYUNJI@users.noreply.github.com>
2025-03-06 23:46:49 +0900

    수포자문제.java
    배열을 이용해 푸는 완전 탐색문제
    패턴이 정해져 있음으로 문제를 자세히 분석할 것

---
+++ 자료구조 및 알고리즘 문제/완전 탐색_배열.java
@@ -0,0 +1,19 @@
+package Testsolution;
+
+public class Test04 {
+	// 수포자 문제 
+	// 1번 문제부터 마지막 문제까지 답이 들은 배열 = answer
+	// 가장 많은 문제를 맞힌 사람이 누구인지, 
+	// 동일 점수사람들 오름차순으로 정렬
+	class Solution{
+		public static int[] solution(int[] answers) {
+			// 수포자 패턴
+			int[][] pattern = {
+					{1,2,3,4,5},{2,1,2,3,2,4,2,5},{3,3,1,1,2,2,4,4,5,5}
+					
+			};
+			return answers;
+		}
+	}
+	
+}
