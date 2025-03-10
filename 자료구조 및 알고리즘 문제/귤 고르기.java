71c02e8f56d12d590ffb9e5c7d417484cb450419
happy <45088924+LIMYUNJI@users.noreply.github.com>
2025-03-10 23:56:42 +0900

    귤 고르기
    귤고르기 문제- 그리디(탐욕) 알고리즘을 사용한 구현
    중복이 없어야 하기 때문에 HashMap 이용하여 문제풀이

---
+++ 자료구조 및 알고리즘 문제/귤 고르기.java
@@ -0,0 +1,26 @@
+package greed;
+
+// 귤 문제 HashMap(중복X) 을 사용하거나 배열로 구현
+/* 수확한 귤중 K개를 골라 상자에 담아 판매하려고 함
+ * 귤을 크리별로 분류했을 때 서로 다른 종류의 수를 최소화
+ * 
+ * 큘의 크기가 8개[1,2,3,4,5,5,2,3], 경화가 귤을 6개 판매 하고 싶다면,
+ * 크기가 1, 4인귤을 제외한 6개의 귤을 상자에 담으면
+ * 귤의 크기와 종류가 2,3,5로 총3가지가 되며, 서로 다른 종류가 최소
+ * 
+ * 한상자 기준
+ * 귤의 개수 k개, 귤의 크키를 담은 배열 tangerine가 매개변수
+ * 귤 k개를 고를 때 크기가 서로 다른 종류의 수 최솟값을 return하는 solution 함수를 작성하시오
+ * 
+ * 
+ * */
+public class greedTest03 {
+	class Solution{
+		public int solution(int[] tangerine, int k) {
+			int result = 0;
+			
+			return result;
+		}
+	}
+	
+}
