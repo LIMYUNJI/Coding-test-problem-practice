203714028e5ed0331136ec1df0b8a764dd15e990
happy <45088924+LIMYUNJI@users.noreply.github.com>
2025-03-11 23:09:58 +0900

    귤 고르기
    귤고르기 문제풀이 그리디 알고리즘을 이용하여 문제를 풀면됨
    그리고 중복이 없다고 했음으로 HashMap을 이용하여 문제를 풀이함

--- 자료구조 및 알고리즘 문제/귤 고르기.java
+++ 자료구조 및 알고리즘 문제/귤 고르기.java
@@ -1,5 +1,9 @@
 package greed;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
 // 귤 문제 HashMap(중복X) 을 사용하거나 배열로 구현
 /* 수확한 귤중 K개를 골라 상자에 담아 판매하려고 함
  * 귤을 크리별로 분류했을 때 서로 다른 종류의 수를 최소화
@@ -12,15 +16,44 @@ package greed;
  * 귤의 개수 k개, 귤의 크키를 담은 배열 tangerine가 매개변수
  * 귤 k개를 고를 때 크기가 서로 다른 종류의 수 최솟값을 return하는 solution 함수를 작성하시오
  * 
- * 
+ * k = 6 tangerine = [1,3,2,5,4,5,2,3] result = 3
+ * k = 4 tangerine = [1,3,2,5,4,5,2,3] result = 2
+ * k = 2 tangerine = [1,1,1,1,2,2,2,3] result = 1
  * */
 public class greedTest03 {
-	class Solution{
+	static class Solution{
 		public int solution(int[] tangerine, int k) {
-			int result = 0;
+			// 크기별 개수 파악
+			HashMap<Integer, Integer> map = new HashMap<>();
+			for(int i: tangerine) {
+				// tangerine 에 i가 있다면 +1을 해서 다시 넣어줍니다.
+				map.put(i, map.getOrDefault(i,0)+1);
+			}
+			
+			// 내림차순
+			ArrayList<Integer> keyset = new ArrayList<>(map.values());
+			keyset.sort(Collections.reverseOrder());
+			
+			int result = 0; // 현재 귤의 종류
+			int numSum =0; // 현재 귤의 합
+			
+			for(int count : keyset){
+				numSum += count; 
+				result++;
+				if(numSum >= k) {
+					break;
+				}
+			}
 			
 			return result;
 		}
+	}
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		int[] tangerine = {1, 3, 2, 5, 4, 5, 2, 3};
+		int k = 4;
+		System.out.println(sol.solution(tangerine, k));
+		
 	}
 	
 }
