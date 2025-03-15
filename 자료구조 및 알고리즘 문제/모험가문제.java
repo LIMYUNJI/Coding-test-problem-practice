cf06aef6d058d740d264506ddd307de0514751af
happy <45088924+LIMYUNJI@users.noreply.github.com>
2025-03-15 23:45:40 +0900

    모험가문제
    그리디 문제
    공백을 기준으로 구분하기 때문에 StringTokenizer를 사용함

---
+++ 자료구조 및 알고리즘 문제/모험가문제.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution{
+  public static void main(String[] args) throws IOException{
+    
+    BufferedReader br = new BufferReader(new InputSteamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringTokenizer st = new StringTokenizer();
+    
+  }
+}
+  
