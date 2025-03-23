9ba64ecf97734b25ba9c00d84b4a73b7b5e5328c
happy <45088924+LIMYUNJI@users.noreply.github.com>
2025-03-24 00:00:24 +0900

    경주로 건설문제 풀이
    다익스트라 알고리증 이용 문제
    내부 클래스를 이용하여 구현

--- 자료구조 및 알고리즘 문제/경주로 건설문제.java
+++ 자료구조 및 알고리즘 문제/경주로 건설문제.java
@@ -1,4 +1,14 @@
 class Solution {
+    class Node{
+        int x, y, dest, cost;
+
+        Node(int x, int y, int dest, int cost){
+            this.x = x;
+            this.y = y;
+            this.dest = dest;
+            this.cost = cost;
+        }
+    }
     public int solution(int[][] board) {
         int answer = 0;
         return answer;
