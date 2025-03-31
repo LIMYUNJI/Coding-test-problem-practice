26cbef4fb00507217788ebb8b4e59b5a08cb3f0b
happy <45088924+LIMYUNJI@users.noreply.github.com>
2025-04-01 00:29:40 +0900

    번호표 문제.java
    그리디문제 큐를 이용해 풀이함
    번호표가 1부터 시작하는 것 기억할 것

--- 자료구조 및 알고리즘 문제/번호표 문제.java
+++ 자료구조 및 알고리즘 문제/번호표 문제.java
@@ -1,19 +1,23 @@
 import java.util.ArrayDeque;
 
-class Solution{
-  private int solution(int N, int K){ // 번호표(1~N), 없앨 사람의 번호
-    ArrayDeque<Integer> deque = new ArrayDeque<>();
-        for(int i = 0; i <=N; i++;){
-          deque.add(i); // 큐에 요소 추가
-        }
-    while(deque.size() > 1){// 사람이 한명 남을 때 까지
-      for(int 1 = 0; i < k-1; i++;){ // K요소 찾기
-        deque.Lastadd(deque.pollFirst()); 
-      }
-        deque.pollFirst();
-    }
-    retuen deque.pollFirst(); 
-  }
+public class Solution{
+	public static void main(String[] args) {
+		System.out.println(solution(5,2));
+	}
+	
+	private static int solution(int N, int K) { // 번호표(1~N), 없앨 사람의 번호
+		ArrayDeque<Integer> deque = new ArrayDeque<>();
+		for (int i = 1; i <= N; i++) { // 번호표 1부터 시작 함 명심할것
+			deque.add(i); // 큐에 요소 추가
+		}
+		while (deque.size() > 1) {// 사람이 한명 남을 때 까지
+			
+			for (int i = 0; i < K - 1; i++) { // K요소 찾기
+				deque.addLast(deque.pollFirst());
+			}
+			deque.pollFirst();
+		}
+		return deque.pollFirst();
+	}
 
-  
 }
