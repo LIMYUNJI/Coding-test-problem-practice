1f096578c63892ea8ec591a91b22a9ae7cac1340
happy <45088924+LIMYUNJI@users.noreply.github.com>
2025-04-01 00:20:04 +0900

    번호표 문제풀이
    큐를 이용하여 푸는 그리디 문제

--- 자료구조 및 알고리즘 문제/번호표 문제.java
+++ 자료구조 및 알고리즘 문제/번호표 문제.java
@@ -1,1 +1,19 @@
+import java.util.ArrayDeque;
 
+class Solution{
+  private int solution(int N, int K){ // 번호표(1~N), 없앨 사람의 번호
+    ArrayDeque<Integer> deque = new ArrayDeque<>();
+        for(int i = 0; i <=N; i++;){
+          deque.add(i); // 큐에 요소 추가
+        }
+    while(deque.size() > 1){// 사람이 한명 남을 때 까지
+      for(int 1 = 0; i < k-1; i++;){ // K요소 찾기
+        deque.Lastadd(deque.pollFirst()); 
+      }
+        deque.pollFirst();
+    }
+    retuen deque.pollFirst(); 
+  }
+
+  
+}
