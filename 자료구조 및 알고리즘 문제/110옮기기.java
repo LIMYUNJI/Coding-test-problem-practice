0e2bdeef0cbcfe67e82f215c3033d5d5483c5ff7
happy <45088924+LIMYUNJI@users.noreply.github.com>
2025-04-01 23:52:33 +0900

    110옮기기.java
    110옮기기 문제 풀이

---
+++ 자료구조 및 알고리즘 문제/110옮기기.java
@@ -0,0 +1,14 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+
+class Solution{
+  public static void main{
+    System.out.println(Arrays.toString(solution(new String[] {"1110","100111100","0111111010"})));
+    System.out.println(Arrays.toString(solution(new String[] {"1011110","01110","101101111010"})));
+    System.out.println(Arrays.toString(solution(new String[] {"10110","100111100","0111111010"})));
+    
+  }
+
+  public static String[] soltion(String[] s){
+  }
+}
