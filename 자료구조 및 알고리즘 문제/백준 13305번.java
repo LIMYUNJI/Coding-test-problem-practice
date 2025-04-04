d22f7dc0b33b79e7bd75a558f27913a5acc71bd0
happy <45088924+LIMYUNJI@users.noreply.github.com>
2025-04-05 00:10:55 +0900

    백준 13305번 문제풇이

---
+++ 자료구조 및 알고리즘 문제/백준 13305번.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+  public static int N;
+  public static int cost;
+  public static int MIN;
+  public static int[] road;
+  public static int[] oil;
+
+  public static void main(String[] args) throws IOException{
+      BufferedReader br = new BufferdReader(new InputStreamReader(System.in));
+      StringTokenizer st;
+
+      N = Integer.parseInt(br.readLine());
+
+      road = new int[N - 1];
+      oil = new int[N];
+    
+  }
+}
