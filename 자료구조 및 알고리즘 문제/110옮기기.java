347ea9019d0d2bc63501bc9cc5f1fea2a8838492
happy <45088924+LIMYUNJI@users.noreply.github.com>
2025-04-04 23:54:31 +0900

    110옮기기

--- 자료구조 및 알고리즘 문제/110옮기기.java
+++ 자료구조 및 알고리즘 문제/110옮기기.java
@@ -2,14 +2,14 @@ import java.util.ArrayDeque;
 import java.util.Arrays;
 
 class Solution{
-  public static void main{
+  public static void main(String[] args){
     System.out.println(Arrays.toString(solution(new String[] {"1110","100111100","0111111010"})));
     System.out.println(Arrays.toString(solution(new String[] {"1011110","01110","101101111010"})));
     System.out.println(Arrays.toString(solution(new String[] {"10110","100111100","0111111010"})));
     
   }
 
-  public static String[] soltion(String[] s){
+  public static String[] solution(String[] s){
     String[] answer = new String[s.length];
 
     for(int i = 0; i < s.length; i++){
@@ -23,9 +23,30 @@ class Solution{
           char s3 = stack.pop();
           char s2 = stack.pop();
           char s1 = stack.pop();
-          
+          if( ("" +s1 + s2 + s3).equals("110")){
+            start.append("110");
+          } else {
+            stack.push(s1);
+            stack.push(s2);
+            stack.push(s3);
+            
+          }
         }
       }
-    }
+      StringBuilder ans = new StringBuilder();
+      while(!stack.isEmpty()){
+        ans.append(stack.pollLast());
+      }
+      if(ans.indexOf("11") >= 0){
+        ans.insert(ans.indexOf("11"), start);
+      } else if(ans.indexOf("0") >= 0){
+        // lastIndeexOf
+        ans.insert(ans.lastIndexOf("0")+1, start);
+    } else {
+        ans.insert(0, start);
+  }
+      answer[i] = ans.toString();
+}
+return answer;
   }
 }
