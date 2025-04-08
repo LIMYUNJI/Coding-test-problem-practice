94df150c9c96103bfb2749b57837fbb26d78e5dc
happy <45088924+LIMYUNJI@users.noreply.github.com>
2025-04-08 23:24:07 +0900

    여행경로.java
    back트래킹을 이용한 문제

---
+++ 자료구조 및 알고리즘 문제/여행경로.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Solution{
+  // 항공권 정보 2차원 배열 tickets 매개변수, 방문 하는 경로를 배열에 담아 return
+  // 제한 사항
+  // 모든 공항은 알파벳 대문자 3글자 
+  // 주어진 공항의 수는 3개 이상 10,000이하
+  // tickets의 각 행 [a, b]는 a 공항에서 b 공항으로 가는 항공권이 있다는 의미
+  // 주어진 항공권은 모두 사용할 것
+  // 만일 가능한 경로가 2개 이상일 경우 알파벳 순서가 앞서는 경로를 return 
+  // 모든 도시를 방문할 수 없는 경우는 주어지지 않음
+  
+  int n;
+  boolean[] visited;
+  ArrayList<String> arr = new ArrayList<>();
+
+  
+  public String[] solution(String[][] tickets){
+    
+  }
+}
