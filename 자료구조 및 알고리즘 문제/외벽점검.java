34797993430866de81a6d930fa37eb9492f86663
happy <45088924+LIMYUNJI@users.noreply.github.com>
2025-04-14 22:54:11 +0900

    외벽점검
    백트래킹을 이용한 문제

---
+++ 자료구조 및 알고리즘 문제/외벽점검.java
@@ -0,0 +1,27 @@
+class Solution{
+  private static int n;
+  private static int[] Weak;
+  private static boolean visited;
+  // n: 외벽의 길이, weak 취약지점의 위치, dist 친구들이 이동할 수 있는 거리
+
+  //  입력받은 친구들이 외벽 점검이 가능한지
+  private static boolean check(int[] dist){
+    // 외벽을 0 ~ 외벽길이 까지 확인
+    for(int i = 0; i < n; i++){
+      
+    }
+      if(){  
+        return true;
+  }
+        return false;
+  }
+
+  // 백트래킹
+  public static void back(int n, int[] dist, int[] org){
+    
+  }
+  public int solution(int n, int[] weak, int[] dist){
+    
+    return result;
+  }
+}
