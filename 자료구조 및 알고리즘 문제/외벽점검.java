ae4e71aac7c2003faac9f3dab1a66cb6aa5f4de3
happy <45088924+LIMYUNJI@users.noreply.github.com>
2025-04-16 00:22:12 +0900

    외벽점검
    순열과 왑전탐색(백트래킹)을 이용한 문제 풀이

--- 자료구조 및 알고리즘 문제/외벽점검.java
+++ 자료구조 및 알고리즘 문제/외벽점검.java
@@ -1,8 +1,8 @@
 class Solution{
-  private static int size = 0;
-  private static int[] Weak;
+  private static int size = 0; // 외변의 크기
+  private static int[] weak, dist;
   private static boolean visited;
-  private static List<List<Integer>> a = new ArrayList<>():
+  private static List<List<Integer>> a = new ArrayList<>(); // 모든 순서를 담을 배
   // n: 외벽의 길이, weak 취약지점의 위치, dist 친구들이 이동할 수 있는 거리
 
   //  입력받은 친구들이 외벽 점검이 가능한지
@@ -17,10 +17,25 @@ class Solution{
         return false;
   }
 
-  // 백트래킹
-  public static void back(int n, int[] dist, int[] org){
-    
+  // 백트래킹을 이용한 친구들 위치 순서에 대한 모든 경우의 수 구하기
+  public static void back(List<Integer>list){
+    if(list.size()> dist.length){
+      a.add(list);
+      return;
+    }
+    for(int i =0; i <dist.length; i++){
+      if(!visted[i]){
+        continue;
+        visited[i] = true;
+        list.add(dist[i]);
+        // 재귀
+        back(new ArrayList<Integer> list))
+        visited[i] = false;
+        list.remove(list.size()-1);
+      }
+    }
   }
+  
   public int solution(int n, int[] weak, int[] dist){
     Weak = weak;
     return result;
