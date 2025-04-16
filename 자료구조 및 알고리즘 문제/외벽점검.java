087891533ec412632680bab12bb01edae502406d
happy <45088924+LIMYUNJI@users.noreply.github.com>
2025-04-16 23:52:53 +0900

    외벽점검

--- 자료구조 및 알고리즘 문제/외벽점검.java
+++ 자료구조 및 알고리즘 문제/외벽점검.java
@@ -1,12 +1,13 @@
 class Solution{
   private static int size = 0; // 외변의 크기
+  private static int n;
   private static int[] weak, dist;
-  private static boolean visited;
+  private static boolean[] visited;
   private static List<List<Integer>> a = new ArrayList<>(); // 모든 순서를 담을 배
   // n: 외벽의 길이, weak 취약지점의 위치, dist 친구들이 이동할 수 있는 거리
 
   //  입력받은 친구들이 외벽 점검이 가능한지
-  private static boolean check(int[] dist){
+  private static boolean check(int[] order){
     // 외벽을 0 ~ 외벽길이 까지 확인
     for(int i = 0; i < n; i++){
       
@@ -19,19 +20,19 @@ class Solution{
 
   // 백트래킹을 이용한 친구들 위치 순서에 대한 모든 경우의 수 구하기
   public static void back(List<Integer>list){
-    if(list.size()> dist.length){
+    if(list.size() == dist.length){
       a.add(list);
       return;
     }
-    for(int i =0; i <dist.length; i++){
+    for(int i =0; i < dist.length; i++){
       if(!visted[i]){
         continue;
         visited[i] = true;
         list.add(dist[i]);
-        // 재귀
-        back(new ArrayList<Integer> list))
-        visited[i] = false;
+        back(list)
         list.remove(list.size()-1);
+        visited[i] = false;
+        
       }
     }
   }
