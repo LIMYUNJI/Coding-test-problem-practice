29b11e26dccb074db2326fb7fc1f9dcb0ef0fce0
happy <45088924+LIMYUNJI@users.noreply.github.com>
2025-04-30 23:25:37 +0900

    타겟넘버(DFS)
    재귀함수를 이용하여 문제 풀이

---
+++ 자료구조 및 알고리즘 문제/타겟넘버.java
@@ -0,0 +1,20 @@
+class Solution {
+    int answer = 0;
+    public void dfs(int index, int sum, int[] numbers, int target){
+        if(index == numbers.length){
+            if(sum == target){
+                answer++;
+        }
+            return;
+    }
+        dfs(index + 1, sum + numbers[index], numbers, target); // 플러스
+        dfs(index + 1, sum - numbers[index], numbers, target); // 마이너스
+    }
+     
+    public int solution(int[] numbers, int target) {
+        answer = 0;
+        dfs(0, 0, numbers, target);
+        return answer;
+    }
+}
+
