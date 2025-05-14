6ca39860d84a3d8e626d8f7b5865e9c8d494f7a1
happy <45088924+LIMYUNJI@users.noreply.github.com>
2025-05-14 23:56:06 +0900

    n^2 배열 자르기

---
+++ 프로그래머스 문제 풀이/n^2 배열 자르기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] solution(int n, long left, long right) {
+        int[] answer = new int[(int)(right - left)+1];
+        // 2차원 배열에서 좌표로 변화
+        int count = 0;
+        
+        return answer;
+    }
+}
