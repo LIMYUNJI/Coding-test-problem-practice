71c13bee6b235260bd2e3b3e6363c6d112f9d32a
happy <45088924+LIMYUNJI@users.noreply.github.com>
2025-06-09 23:54:49 +0900

    롤케이크 자르기
    Map, set을 이용하는 방법보다 배열로 풀이하는게 더 효율적이라고 판단

--- 프로그래머스 문제 풀이/롤케이크 자르기.java
+++ 프로그래머스 문제 풀이/롤케이크 자르기.java
@@ -1,6 +1,34 @@
 class Solution {
     public int solution(int[] topping) {
-        int answer = -1;
+        int answer = 0;
+
+        int [] soo = new int [10001]; // 철수가 가지는 토핑
+        int [] bro1 = new int [10001]; // 동생이 가지는 토핑
+
+        
+        int soo_cnt = 0; // 철수 토핑 종류
+        int bro1_cnt = 0;// 동생 토핑종류
+
+        for(int i = 0; i < topping.length; i++){
+            bro1[topping[i]]++;
+            if(bro1[topping[i]] == 1) bro1_cnt++;
+        }
+        for(int i = 0; i <topping.length; i++){
+            if(){
+                soo_cnt++;
+                bro1_cnt--;
+            }
+            if(){
+                soo_cnt--;
+                bro1_cnt++;
+            }
+            
+            if(soo_cnt == bro1_cnt){
+                answer++;
+            }
+        }
+        
+    }
         return answer;
     }
 }
