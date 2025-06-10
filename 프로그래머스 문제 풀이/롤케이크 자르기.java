4718cc4a31b7d8f4d9bdc3122077ca9cc8587946
happy <45088924+LIMYUNJI@users.noreply.github.com>
2025-06-10 23:55:08 +0900

    롤케이크 자르기
    토핑 분배 관련 로직 추가

--- 프로그래머스 문제 풀이/롤케이크 자르기.java
+++ 프로그래머스 문제 풀이/롤케이크 자르기.java
@@ -1,3 +1,9 @@
+/*
+soo[t]가 1이 될 때만 철수 토핑 종류 수를 1 증가
+bro1[t]가 0이 될 때 동생 토핑 종류 수를 1 감소
+토핑을 하나씩 옮기면서 종류 수를 비교해 정답에 더함
+*/
+
 class Solution {
     public int solution(int[] topping) {
         int answer = 0;
@@ -7,20 +13,28 @@ class Solution {
 
         
         int soo_cnt = 0; // 철수 토핑 종류
-        int bro1_cnt = 0;// 동생 토핑종류
+        int bro1_cnt = 0;// 동생 토핑 종류
 
+         // 동생이 모든 토핑을 먼저 가진 상태 세팅
         for(int i = 0; i < topping.length; i++){
             bro1[topping[i]]++;
-            if(bro1[topping[i]] == 1) bro1_cnt++;
+            if(bro1[topping[i]] == 1) {
+                bro1_cnt++;
+            }
         }
         for(int i = 0; i <topping.length; i++){
-            if(){
+            int t = topping[i];
+
+            // 철수 토핑 추가
+                soo[t]++;
+            if( soo[t] == 1){ // 토핑 종류 추가 되는 경
                 soo_cnt++;
-                bro1_cnt--;
             }
-            if(){
-                soo_cnt--;
-                bro1_cnt++;
+
+            // 철수 토핑이 증가 하므로 동생 토핑은 감소
+                bro1[t]--;
+            if( bro1[t] == 0){ // 동생 토핑 감
+                bro1_cnt--;
             }
             
             if(soo_cnt == bro1_cnt){
@@ -28,7 +42,6 @@ class Solution {
             }
         }
         
-    }
         return answer;
     }
 }
