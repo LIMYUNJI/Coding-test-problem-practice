12a44f1a55fc97478ec6c229975aab8182e7d39e
happy <45088924+LIMYUNJI@users.noreply.github.com>
2025-06-14 23:11:59 +0900

    올바른 괄호
    스택을 알고리즘을 이해하는 기본문제

--- 자료구조 및 알고리즘 문제/올바른 괄호.java
+++ 자료구조 및 알고리즘 문제/올바른 괄호.java
@@ -1,1 +1,18 @@
+import java.util.ArrayDeque;
 
+class Solution {
+  private boolean solution(String s) {
+        
+        ArrayDeque<Character> stack = new ArrayDeque<Character>();
+        
+        char[] a = s.toCharArray(); // 문자열을 문자 배열로 변환
+         for(char c : a){
+           if(c == '('){
+            stack.push(c);
+           } else if(stack.isEmpty()||stack.pop() == ')') { // 스택이 비어있는지 확인
+               return false;
+         }
+        }
+        return stack.isEmpty();
+    }
+}
