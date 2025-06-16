b44146069a5d39dc16df342850454c022e3422ae
happy <45088924+LIMYUNJI@users.noreply.github.com>
2025-06-16 23:59:38 +0900

    다단계 칫솔판매

---
+++ 자료구조 및 알고리즘 문제/다단계 칫솔판매.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+
+class Solution{
+  public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount){
+    HashMap<String, String> parent = new HashMap<>();
+
+    for(int i  = 0; i < enroll.length; i++){
+      parent.put(enroll[i],referral[i);
+    }
+   HashMap<String, Integer> parent = new HashMap<>();
+    for(){
+      
+    }
+  }
+}
