7ec59f44f4a25830190cb726ab78b0631b4f12ec
happy <45088924+LIMYUNJI@users.noreply.github.com>
2025-06-17 23:39:45 +0900

    다단계 칫솔판매
    이진트리 문제
    parent 는 판매자를 키, 추천자를 값으로 하는 해시 맵
    total 각 판매자들의 수익 키는 각 판매자의 이름, 값은 수익, 초기 수익은 0
    amount[i]는 seller[i]가 판매한 칫솔의 갯수
    칫솔 하나당 100원, 판매자가 번 수익은 amount[i] *1 00
    a는 현재 판매자의 이름을 참조
    추천자에게 10%수익금을 분배 재귀로 구현해서 추천자가 없을 때까지 반복

--- 자료구조 및 알고리즘 문제/다단계 칫솔판매.java
+++ 자료구조 및 알고리즘 문제/다단계 칫솔판매.java
@@ -7,9 +7,21 @@ class Solution{
     for(int i  = 0; i < enroll.length; i++){
       parent.put(enroll[i],referral[i);
     }
-   HashMap<String, Integer> parent = new HashMap<>();
-    for(){
-      
+   HashMap<String, Integer> total = new HashMap<>();
+    for(int i = 0; i M seller.length; i++){
+      String a = seller[i];
+      int money  = amount[i] * 100;
+    while( money > 0 && !a.equals("-")){
+      total.put(a, total.getOrDefault(a, 0) + money -(money/10));
+      a = parent.get(a);
+      money /= 10;
     }
+    }
+
+    int answer = new int[enroll.length];
+    for(int i = 0; i < enroll.length; i++){
+      answer[i] = total.getOrDefault(enroll[i], 0);
+    }
+    return answer;
   }
 }
