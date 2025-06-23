fd2fcdb85c15bb0c33bf6dd1ce68a40586c05b33
happy <45088924+LIMYUNJI@users.noreply.github.com>
2025-06-23 23:01:20 +0900

    게임맵 최단 거리
    넓이 우선 탐색BFS 문제 풀이

---
+++ 자료구조 및 알고리즘 문제/게임맵 최단 거리.java
@@ -0,0 +1,28 @@
+improt java.util.ArrayDeque;
+// BFS 문제 풀이
+
+// 완주할때까지 움직인 칸수 return , 완주를 못하는 걍우 -1 return 
+
+class Solution {
+    int[] x = {1 ,-1 ,0 ,0};
+    int[] y = {0, 0, 1, -1};
+
+  static class Node {
+    int co, ro;
+    Node(int r, int c){
+      this.r = r;
+      this.c = c;
+    }
+  }
+    public int solution(int[][] maps) {
+      int n = maps.length;
+      int m = maps[0].length;
+
+      int [][] dist = new int [x][y]; 
+      ArrayDeque<Interger> queue = new ArrayDeque<>();
+
+      // 넓이 우선 탐색 구현
+      int answer = 0;
+        return answer;
+    }
+}
