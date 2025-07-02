5226f9bfd105fd22348812c76f8df74743495b17
happy <45088924+LIMYUNJI@users.noreply.github.com>
2025-07-03 00:11:46 +0900

    네트워크

--- 자료구조 및 알고리즘 문제/네트워크.java
+++ 자료구조 및 알고리즘 문제/네트워크.java
@@ -1,4 +1,29 @@
 class Solution{
+    private static boolean[] visit;
+    private static int[][] com;
 
+    private static void dfs(int now){
+      visit[now] = true;
+      for(int i = 0; i<com[now].length; i++){
+        if(com[now][i] == 1 && !visit[i]){
+          dfs(i);
+        }
+      }
+    }
+
+  public int solution(int n, int [][] coms){
+    int result = 0;
+    com = coms;
+    visit = new boolean[n];
+
+    for(int i = 0; i< n; i++){
+      if(!visit[i]){
+        dfs(i);
+        result++;
+      }
+    }
+
+    return result;
+  }
   
 }
