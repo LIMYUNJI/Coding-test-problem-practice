007fab9759a18b5a83e7c0af4b7076514939cfbb
happy <45088924+LIMYUNJI@users.noreply.github.com>
2025-07-10 23:59:08 +0900

    과일장수

---
+++ 프로그래머스 문제 풀이/과일장수.java
@@ -0,0 +1,13 @@
+// 사과의 최대 점수 K 최하점은 1
+// 한 상자에 들어가는 사과의 수 m
+// 사과들의 점수 score
+// (최저 사과 점수) x (한 상자에 담긴 사과 개수) x (상자의 개수) = 2 x 4 x 1 = 8
+// 상자에 담긴 사과 중 가장 낮은 점수가 p (1 ≤ p ≤ k)점인 경우, 사과 한 상자의 가격은 p * m
+// 과일 장수가 얻을 수 있는 최대 이익을 구하시오
+// * 사과는 상자 단위로만 판매하며 남는 사과는 버림
+class Solution {
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+        return answer;
+    }
+}
