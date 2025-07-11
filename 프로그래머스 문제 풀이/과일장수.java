1c7f800c4faf4e71bb6e47d6f9ccc6f11e77896f
happy <45088924+LIMYUNJI@users.noreply.github.com>
2025-07-11 23:59:55 +0900

    과일장수
    그리디 알고리즘

--- 프로그래머스 문제 풀이/과일장수.java
+++ 프로그래머스 문제 풀이/과일장수.java
@@ -5,9 +5,16 @@
 // 상자에 담긴 사과 중 가장 낮은 점수가 p (1 ≤ p ≤ k)점인 경우, 사과 한 상자의 가격은 p * m
 // 과일 장수가 얻을 수 있는 최대 이익을 구하시오
 // * 사과는 상자 단위로만 판매하며 남는 사과는 버림
+
+// 그리디 알고리즘
+// 유망하수: 박스를 포장해 팔 때, 가장 점수가 낮은 사과 기준으로 박스를 만
 class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
+
+            for(int i = 0; i < score.length; i++){
+            score[i] = 
+        }
         return answer;
     }
 }
