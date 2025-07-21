04e93d61ee83368207f1fc91120a931002d26387
happy <45088924+LIMYUNJI@users.noreply.github.com>
2025-07-21 23:59:54 +0900

    길찾기게임
    이진트리이용

--- 자료구조 및 알고리즘 문제/길찾기게임.java
+++ 자료구조 및 알고리즘 문제/길찾기게임.java
@@ -6,6 +6,20 @@ class Solution {
     int x, y, num;
     Node left, reight;
 
-    
+    public Node(int x, int y, int num){
+      this.x = x;
+      this.y = y;
+      this.num = num;
+    }
   }
+   List<Integer> preorderList = new ArrayList<>();
+   List<Integer> postorderList = new ArrayList<>();
+
+    public int[][] solution(int[][] nodeinfo) {
+        int n = nodeinfo.length;
+        Node[] nodes = new Node[n];
+
+        for (int i = 0; i < n; i++) {
+            nodes[i] = new Node(nodeinfo[i][0], nodeinfo[i][1], i + 1);
+        }
 }
