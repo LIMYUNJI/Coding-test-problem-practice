37261f8ab981a9466e087189a93be77a18abae0f
happy <45088924+LIMYUNJI@users.noreply.github.com>
2025-08-27 23:56:40 +0900

    28278 스택2

--- 28278 스택2.java
+++ 28278 스택2.java
@@ -1,9 +1,34 @@
 import java.util.ArrayDeque;
+import java.io.*;
+
 
 public class Main{
   public static void main(String[] args){
-    ArrayDeque<Integer> stack = new ArrayDeque<>();
-  } 
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      StringBuilder sb = new StringBuilder();
+      int N = Integer.parseInt(br.readLine());
+    
+      ArrayDeque<Integer> stack = new ArrayDeque<>();
+  
+       for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            StringTokenizer st = new StringTokenizer(line);
+            String token = st.nextToken();
 
+            if (token.equals("1")) {
+                int x = Integer.parseInt(st.nextToken());
+                stack.push(x);
+            } else if (token.equals("2")) {
+                sb.append(stack.isEmpty() ? -1 : stack.pop()).append('\n');
+            } else if (token.equals("3")) {
+                sb.append(token.size()).append('\n');
+            } else if (token.equals("4")) { 
+                sb.append(token.isEmpty() ? 1 : 0).append('\n');
+            } else if (token.equals("5")) {
+                sb.append(stack.isEmpty() ? -1 : stack.peek()).append('\n');
+            }
+        }
+        System.out.print(sb);
+  } 
   
 }
