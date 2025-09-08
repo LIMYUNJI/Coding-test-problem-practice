7736f833f21243ec05cd82644d0dd4334db1f8ba
happy <45088924+LIMYUNJI@users.noreply.github.com>
2025-09-08 23:48:26 +0900

    백준 9935

--- 백준 9935.java
+++ 백준 9935.java
@@ -1,1 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
+
+public class Main{
+    static char[] origin;
+    static char[] bomb;
+  public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        origin = br.readLine().toCharArray();
+        bomb = br.readLine().toCharArray();
+        int bombIdx = bomb.length - 1;
+        ArrayDeque<Character> deque = new ArrayDeque<Character>();
+  }
+}
