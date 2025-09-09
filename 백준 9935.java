6b8735c51560b1b7c117385f634cd0b03153da78
happy <45088924+LIMYUNJI@users.noreply.github.com>
2025-09-09 23:59:58 +0900

    Update 백준 9935

--- 백준 9935.java
+++ 백준 9935.java
@@ -1,25 +1,54 @@
+/*(주어진 문자열에서 특정 "폭발 문자열(bomb)"이 나오면 그 부분을 지우고, 없을 때까지 반복하는 문제*/
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.ArrayDeque;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] origin = br.readLine().toCharArray();
+        char[] bomb = br.readLine().toCharArray();
+        int bombLen = bomb.length;
 
-public class Main{
-    static char[] origin;
-    static char[] bomb;
-  public static void main(String[] args) throws IOException{
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        origin = br.readLine().toCharArray();
-        bomb = br.readLine().toCharArray();
-        int bombIdx = bomb.length - 1;
-        ArrayDeque<Character> stack = new ArrayDeque<Character>();
+        ArrayDeque<Character> stack = new ArrayDeque<>();
 
-      for(int i = 0; i< origin.length; i++){
-          if(){
-          
-          } else if() { // bomb 같은 문자가 2개 겹치는 경우
-          
-      }
-  }
+        for (char c : origin) {
+            stack.addLast(c); // 스택에 문자 추가
+
+            // 폭발 문자열 길이만큼 스택의 끝을 확인
+            if (stack.size() >= bombLen) {
+                boolean isBomb = true;
+                int idx = bombLen - 1;
+
+                for (Character ch : new ArrayDeque<>(stack).descendingIterator()) {
+                    if (bomb[idx] != ch) {
+                        isBomb = false;
+                        break;
+                    }
+                    idx--;
+                    if (idx < 0) break;
+                }
+
+                // 폭발 문자열과 같다면 제거
+                if (isBomb) {
+                    for (int i = 0; i < bombLen; i++) {
+                        stack.removeLast();
+                    }
+                }
+            }
+        }
+
+        // 남은 문자들을 출력
+        if (stack.isEmpty()) {
+            System.out.println("FRULA");
+        } else {
+            StringBuilder sb = new StringBuilder();
+            for (char c : stack) {
+                sb.append(c);
+            }
+            System.out.println(sb);
+        }
+    }
 }
