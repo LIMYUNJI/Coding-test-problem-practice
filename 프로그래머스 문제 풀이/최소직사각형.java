59ff72d02e3a2d67d2f42fccdbb49fa9a23bc915
happy <45088924+LIMYUNJI@users.noreply.github.com>
2025-11-13 23:58:57 +0900

    Start implementation of solution method
    Implement the solution method for rectangle problem.

--- 프로그래머스 문제 풀이/최소직사각형.java
+++ 프로그래머스 문제 풀이/최소직사각형.java
@@ -1,6 +1,12 @@
 // 넓이 우선 탐색
+
 class Solution {
     public int solution(int[][] sizes) {
+
+        for(int i = 0; i < sizes.length; i++){
+            
+        }
+        
         int answer = 0;
         return answer;
     }
