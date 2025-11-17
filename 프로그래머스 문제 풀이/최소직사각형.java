67724229311a1d4108497bce76ba7ef2bebb92b4
happy <45088924+LIMYUNJI@users.noreply.github.com>
2025-11-17 23:48:06 +0900

    Refactor solution method to calculate max dimensions

--- 프로그래머스 문제 풀이/최소직사각형.java
+++ 프로그래머스 문제 풀이/최소직사각형.java
@@ -2,12 +2,18 @@
 
 class Solution {
     public int solution(int[][] sizes) {
+        int maxWidth = 0;
+        int maxHeight = 0;
 
-        for(int i = 0; i < sizes.length; i++){
-            
+        for (int[] card : sizes) {
+            int w = Math.max(card[0], card[1]);
+            int h = Math.min(card[0], card[1]);
+
+            maxWidth = Math.max(maxWidth, w);
+            maxHeight = Math.max(maxHeight, h);
         }
-        
-        int answer = 0;
-        return answer;
+
+        return maxWidth * maxHeight;
     }
 }
+
