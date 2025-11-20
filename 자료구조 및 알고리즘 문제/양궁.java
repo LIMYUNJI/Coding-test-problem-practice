70297fa940a39cca14c63322a02e5f13eb0d7c21
happy <45088924+LIMYUNJI@users.noreply.github.com>
2025-11-20 23:59:34 +0900

    Initialize Solution class with comments for backtracking
    Added comments and structured the initial setup for the backtracking and combination logic to determine the optimal arrow distribution for winning.

--- 자료구조 및 알고리즘 문제/양궁.java
+++ 자료구조 및 알고리즘 문제/양궁.java
@@ -1,4 +1,12 @@
-// 
-class Solution{
-  
+// 백트래킹 + 조합
+// 유망함수 : 
+// 라이언이 가장 큰 점수 차이로 우승하기 위해 n 발의 화살을 어떤 과녁들에 맞춰야 하는지
+// 순서대로 정수 배열에 담아 retun, 우승 할 수 없는 경우(지거나, 비기는 경우 포함) -1 return
+class Solution {
+  int[] rayn = new int[11];
+
+  // 조합을 통해서 각각의 점수를 계산하는 코드
+  private static int getScore(){
+    
+  }
 }
