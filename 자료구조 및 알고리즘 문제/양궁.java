0f972cd41cb991dfc761b9b5e4b5c456eda939d0
happy <45088924+LIMYUNJI@users.noreply.github.com>
2025-11-21 00:03:55 +0900

    설명 및 풀이과정 과정 추가

--- 자료구조 및 알고리즘 문제/양궁.java
+++ 자료구조 및 알고리즘 문제/양궁.java
@@ -4,9 +4,18 @@
 // 순서대로 정수 배열에 담아 retun, 우승 할 수 없는 경우(지거나, 비기는 경우 포함) -1 return
 class Solution {
   int[] rayn = new int[11];
-
+  int[] apeach = new int[11];
+  int maxScore = 0; // 라이언이 어피치 보다 얻을 점수의 max값
+  
   // 조합을 통해서 각각의 점수를 계산하는 코드
-  private static int getScore(){
-    
+  private static int getScore(int[] ryan){
+    int score = 0;
+    for(int i = 0; i <=10; i++){
+      if(rayn[i[ + apeach[1] > 0){
+        // 삼항연산자
+      }
+      
+    }
+    return score;
   }
 }
