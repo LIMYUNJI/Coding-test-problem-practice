f4bfc5a5521adef4737071b13b6f91952483bfb8
happy <45088924+LIMYUNJI@users.noreply.github.com>
2025-11-28 23:59:52 +0900

    양꼬치 문제풀이

--- 프로그래머스 문제 풀이/양꼬치.java
+++ 프로그래머스 문제 풀이/양꼬치.java
@@ -1,1 +1,10 @@
 
+class Solution {
+    public int solution(int n, int k) {
+        int meatPrice = n * 12000;              // 양꼬치 비용
+        int freeDrink = n / 10;                 // 서비스 음료 개수
+        int payDrink = Math.max(0, k - freeDrink); // 실제 돈 내는 음료 개수
+
+        return meatPrice + payDrink * 2000;
+    }
+}
