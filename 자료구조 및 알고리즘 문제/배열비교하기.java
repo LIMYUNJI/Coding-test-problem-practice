6c424c41696eeb259c5832bf9cf649c09a0bc9b6
happy <45088924+LIMYUNJI@users.noreply.github.com>
2025-12-03 23:55:18 +0900

    배열비교하기 문제풀이
    Implement comparison logic for two integer arrays based on their lengths and sums.

---
+++ 자료구조 및 알고리즘 문제/배열비교하기.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int[] arr1, int[] arr2) {
+        int sum1 = 0;
+        int sum2 = 0;
+        
+        if (arr1.length != arr2.length){
+            return arr1.length > arr2.length ? 1 : -1;
+          
+        } else {
+            for (int i : arr1) {
+                sum1 += i;
+            }
+            for (int i : arr2) {
+                sum2 += i;
+            }
+            if (sum1 == sum2) {
+                return 0;
+              
+            } else {
+                return sum1 > sum2 ? 1 : -1;
+            }
+        }
+        
+    
