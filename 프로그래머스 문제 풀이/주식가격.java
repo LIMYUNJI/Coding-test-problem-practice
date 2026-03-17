7c5a2716fd5f8ac0afc1ac9140d380515d1544f7
happy <45088924+LIMYUNJI@users.noreply.github.com>
2026-03-17 23:56:33 +0900

    주식가격 문제풀이 추가
    스택을 이용한 문제풀이

--- 프로그래머스 문제 풀이/주식가격.java
+++ 프로그래머스 문제 풀이/주식가격.java
@@ -1,6 +1,26 @@
+import java.util.Stack;
+
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = {};
+        int n = prices.length;
+        int[] answer = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int i = 0; i < n; i++) {
+            // 가격이 떨어진 경우
+            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
+                int idx = stack.pop();
+                answer[idx] = i - idx;
+            }
+            stack.push(i);
+        }
+        
+        // 끝까지 가격이 안 떨어진 경우
+        while (!stack.isEmpty()) {
+            int idx = stack.pop();
+            answer[idx] = n - 1 - idx;
+        }
+        
         return answer;
     }
 }
